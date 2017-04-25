8276f1c78d24e6a27d291700e3c3719235d3c905
Torkil Solheim <torkil.solheim@gmail.com>
2017-04-25 14:57:28 +0200

    init

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "net/http"
+    "os"
+    "bufio"
+    "net/textproto"
+)
+
+func main() {
+        go startWss()
+
+        http.Handle("/", http.FileServer(http.Dir("./static")))
+        http.ListenAndServe(":3000", nil)
+}
+
+const (
+    CONN_HOST = "localhost"
+    CONN_PORT = "3001"
+    CONN_TYPE = "tcp"
+)
+
+var p = fmt.Println
+
+func startWss() {
+    // Listen for incoming connections.
+    listener, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+    if err != nil {
+        p("Error listening:", err.Error())
+        os.Exit(1)
+    }
+
+    //Executed when the application closes.
+    defer listener.Close()
+
+    p("Listening on " + CONN_HOST + ":" + CONN_PORT)
+    for {
+        // Listen for an incoming connection.
+        conn, err := listener.Accept()
+        if err != nil {
+            p("Error accepting: ", err.Error())
+            os.Exit(1)
+        }
+        // Handle connections in a new goroutine.
+        go handler(conn)
+    }
+}
+
+// Handles incoming requests.
+func handler(conn net.Conn) {
+  shakeIt(conn)
+
+  p("Got connection from:",conn)
+
+	bufReader := bufio.NewReader(conn)
+  tp := textproto.NewReader(bufReader)
+
+  var headers []string
+
+  for {
+      line, _ := tp.ReadLine()
+      headers = append(headers, line)
+
+      if line == "" {
+          break
+      }
+  }
+
+  p(headers)
+  conn.Write([]byte("Hei"))
+  conn.Close()
+}
+
+func shakeIt(client net.Conn) {
+  var shake = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
+	client.Write([]byte(shake))
+}
