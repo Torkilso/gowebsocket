6b24c680cd234ca43f5d64f7e3b1a7c8cd9270a5
Torkil Solheim <torkil.solheim@gmail.com>
2017-04-25 17:45:46 +0200

    Added function to parse websocket key from client-headers

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
     "os"
     "bufio"
     "net/textproto"
+    "regexp"
+    "strings"
 )
 
 func main() {
@@ -43,37 +45,72 @@ func startWss() {
             p("Error accepting: ", err.Error())
             os.Exit(1)
         }
-        // Handle connections in a new goroutine.
+        // Handle connections in a new thread (goroutine)
         go handler(conn)
     }
 }
 
 // Handles incoming requests.
 func handler(conn net.Conn) {
-  shakeIt(conn)
+  handshake(conn)
 
-  p("Got connection from:",conn)
+  //for loop? hvis alle skal ha hver sin traad
 
-	bufReader := bufio.NewReader(conn)
-  tp := textproto.NewReader(bufReader)
-
-  var headers []string
-
-  for {
-      line, _ := tp.ReadLine()
-      headers = append(headers, line)
-
-      if line == "" {
-          break
-      }
-  }
-
-  p(headers)
-  conn.Write([]byte("Hei"))
   conn.Close()
 }
 
-func shakeIt(client net.Conn) {
-  var shake = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
-	client.Write([]byte(shake))
+func handshake(client net.Conn) {
+  var key = parseKey(client)
+
+  if(key == "false"){
+    reject(client)
+    return
+  }
+
+  p(key)
+
+}
+
+func parseKey(client net.Conn) string {
+  bufReader := bufio.NewReader(client)
+  tp := textproto.NewReader(bufReader)
+
+  var headers []string
+  var key string
+  var keyFound bool
+
+  for {
+    var line, _ = tp.ReadLine()
+    headers = append(headers, line)
+
+		if line == "" {
+			break
+		}
+  }
+
+  for i := 0; i < len(headers); i++ {
+    s := strings.Split(headers[i], ": ")
+    matchKey, errKey := regexp.MatchString(s[0], "Sec-WebSocket-Key")
+    if errKey != nil {
+      p(errKey)
+    }
+    if matchKey {
+      if(len(s)>1){
+        keyFound = true
+        key = s[1]
+      }
+    }
+  }
+
+  if keyFound {
+    return key
+  } else {
+    return "false"
+  }
+}
+
+func reject(client net.Conn) {
+  var reject = "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nIncorrect request"
+  client.Write([]byte(reject))
+  client.Close();
 }
