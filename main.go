b3c34f8bd159b0b604b793dc8c9b70b164b608bd
tjabejohannes <tjabe_johannes@outlook.com>
2017-04-27 10:26:34 +0200

    decode and encoder

--- main.go
+++ main.go
@@ -71,8 +71,8 @@ func recv_data(client net.Conn){
 	p("LISTEN TO recv_data")
 	reply := make([]byte, 32)
 	client.Read(reply)
-	//decode(reply)
-	fmt.Println("Message Received:", reply)
+	decoded := decode(reply)
+	fmt.Println("Message Received:", decoded)
 	client.Write(reply)
 	//client.Close()
 
@@ -118,21 +118,68 @@ func reject(client net.Conn) {
 	//client.Close();
 }
 
-//funker ikke
-func decode(encoded []byte) []byte{
-	key := encoded[2:6]
-	length := int(encoded[1]-128)
-	encodedMsg := encoded[5:5+length]
-	buff := make([]byte,length)
-	fmt.Println("KEY ",key)
-	fmt.Println("LENGTH",length)
-	fmt.Println("ENCODED", encoded)
-
-	for i := 0; i < length; i++ {
-		buff[i] = (encodedMsg[i] ^ key[i%4])
+//Funnet på nett
+/*
+Første byte inneholder typ beskrivelse
+Andre byte inneholder lengden på dataen fra(/til) klienten
+either two or eight bytes if the length does not fit in the second byte (the second byte is then a code saying how many bytes are used for the length)
+the actual (raw) data
+ */
+func decode (rawBytes []byte) string {
+	var idxMask int
+	if rawBytes[1] == 126 {
+		idxMask = 4
+	} else if rawBytes[1] == 127 {
+		idxMask = 10
+	} else {
+		idxMask = 2
 	}
 
-	fmt.Println("DECODED",buff)
+	masks := rawBytes[idxMask:idxMask + 4]
+	data := rawBytes[idxMask + 4:len(rawBytes)]
+	decoded := make([]byte, len(rawBytes) - idxMask + 4)
 
-	return buff
+	for i, b := range data {
+		decoded[i] = b ^ masks[i % 4]
+	}
+	return string(decoded)
+}
+
+//Ikke testet 27.04.17
+func encode (message string) (result []byte) {
+	rawBytes := []byte(message)
+	var idxData int
+
+	length := byte(len(rawBytes))
+	if len(rawBytes) <= 125 { //one byte to store data length
+		result = make([]byte, len(rawBytes)+2)
+		result[1] = length
+		idxData = 2
+	} else if len(rawBytes) >= 126 && len(rawBytes) <= 65535 { //two bytes to store data length
+		result = make([]byte, len(rawBytes)+4)
+		result[1] = 126 //extra storage needed
+		result[2] = ( length >> 8 ) & 255
+		result[3] = ( length      ) & 255
+		idxData = 4
+	} else {
+		result = make([]byte, len(rawBytes)+10)
+		result[1] = 127
+		result[2] = ( length >> 56 ) & 255
+		result[3] = ( length >> 48 ) & 255
+		result[4] = ( length >> 40 ) & 255
+		result[5] = ( length >> 32 ) & 255
+		result[6] = ( length >> 24 ) & 255
+		result[7] = ( length >> 16 ) & 255
+		result[8] = ( length >> 8 ) & 255
+		result[9] = ( length       ) & 255
+		idxData = 10
+	}
+
+	result[0] = 129 //only text is supported
+
+	// put raw data at the correct index
+	for i, b := range rawBytes {
+		result[idxData+i] = b
+	}
+	return
 }
